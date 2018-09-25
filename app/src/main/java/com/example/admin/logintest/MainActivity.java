ed012750e0094572cad764ce62262e7b206dec8d
Sahil Pawar <pawarsahil94.sp@gmail.com>
2018-09-25 15:09:36 +0530

    Facebook Login/LogOut complete.

--- app/src/main/java/com/example/admin/logintest/MainActivity.java
+++ app/src/main/java/com/example/admin/logintest/MainActivity.java
@@ -7,6 +7,8 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,12 +19,16 @@ import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
+import com.facebook.Profile;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.squareup.picasso.Picasso;
+
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Arrays;
@@ -33,7 +39,10 @@ public class MainActivity extends AppCompatActivity {
     private CallbackManager callbackManager;
     private TextView userFirstName, userLastName, gender, birthDay, eMail, userID;
     private ProgressDialog progressDialog;
+    private LoginButton loginButton;
+    private Button buttonLoginFB, buttonLogOut;
     private ImageView profilePicture;
+    private Profile profile;
     private static final String GRAPH_URL = "https://graph.facebook.com/";
     private URL imageURL;
     private static final String TAG = "MainActivity";
@@ -46,7 +55,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         callbackManager = CallbackManager.Factory.create();
-
         userFirstName = findViewById(R.id.userFirstName);
         userLastName = findViewById(R.id.userLastName);
         profilePicture = findViewById(R.id.profilePicture);
@@ -54,8 +62,10 @@ public class MainActivity extends AppCompatActivity {
         birthDay = findViewById(R.id.birthDay);
         eMail = findViewById(R.id.eMail);
         userID = findViewById(R.id.userID);
+        buttonLoginFB = findViewById(R.id.buttonLoginFB);
+        buttonLogOut = findViewById(R.id.buttonLogOut);
 
-        LoginButton loginButton = findViewById(R.id.facebookLoginButton);
+        loginButton = findViewById(R.id.facebookLoginButton);
         loginButton.setReadPermissions(Arrays.asList("public_profile", "email", "user_gender", "user_birthday"));
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
@@ -63,6 +73,10 @@ public class MainActivity extends AppCompatActivity {
                 progressDialog = new ProgressDialog(MainActivity.this);
                 progressDialog.setMessage("Retrieving Data.....");
                 progressDialog.show();
+
+                buttonLoginFB.setVisibility(View.GONE);
+                loginButton.setVisibility(View.GONE);
+                buttonLogOut.setVisibility(View.VISIBLE);
 
                 String accessToken = loginResult.getAccessToken().getToken();
                 Preferences.setFacebookAccessToken(getApplicationContext(), accessToken);
@@ -92,12 +106,43 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        if (Preferences.getFacebookAccessToken(getApplicationContext()) != null) {
+        profile = Profile.getCurrentProfile().getCurrentProfile();
+        if (profile != null) {
+            buttonLoginFB.setVisibility(View.GONE);
+            loginButton.setVisibility(View.GONE);
+            buttonLogOut.setVisibility(View.VISIBLE);
+            displayUserInfo();
+        } else {
+            buttonLoginFB.setVisibility(View.VISIBLE);
+            loginButton.setVisibility(View.GONE);
+            buttonLogOut.setVisibility(View.GONE);
             displayUserInfo();
         }
 
-    }
+        buttonLoginFB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginButton.performClick();
+            }
+        });
 
+        buttonLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginManager.getInstance().logOut();
+
+                if (profile == null) {
+                    buttonLoginFB.setVisibility(View.VISIBLE);
+                    loginButton.setVisibility(View.GONE);
+                    buttonLogOut.setVisibility(View.GONE);
+                    clearData();
+                }
+            }
+        });
+
+
+        displayUserInfo();
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -115,15 +160,14 @@ public class MainActivity extends AppCompatActivity {
             URL profilePictureUrl = new URL(GRAPH_URL + object.getString("id") + "/picture?width=250&height=250");
             imageURL = profilePictureUrl;
             Preferences.setProfilePicture(getApplicationContext(), imageURL.toString());
-
-            Picasso.with(this).load(imageURL.toString()).into(profilePicture);
-
             Preferences.setFacebookID(getApplicationContext(), object.getString("id"));
             Preferences.setUserFirstName(getApplicationContext(), object.getString("first_name"));
             Preferences.setUserLastName(getApplicationContext(), object.getString("last_name"));
             Preferences.setUserGender(getApplicationContext(), object.getString("gender"));
             Preferences.setUserBirthDate(getApplicationContext(), object.getString("birthday"));
             Preferences.setUserEmail(getApplicationContext(), object.getString("email"));
+
+            displayUserInfo();
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
@@ -141,4 +185,15 @@ public class MainActivity extends AppCompatActivity {
         userID.setText(Preferences.getFacebookID(getApplicationContext()));
         Picasso.with(this).load(Preferences.getProfilePicture(getApplicationContext())).into(profilePicture);
     }
+
+    private void clearData() {
+
+        SharedPreferences sharedPreferences = getSharedPreferences("user_info", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.commit();
+
+        displayUserInfo();
+    }
+
 }
